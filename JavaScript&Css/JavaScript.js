4df2561c86c03d74f9dc47fe0ef1ff4e3dfcde30
deocze <255413163+deocze@users.noreply.github.com>
2026-03-06 08:52:05 -0500

    Add for-loops and fix logic

---
+++ JavaScript&Css/JavaScript.js
@@ -0,0 +1,71 @@
+let humanScore = 0,
+  computerScore = 0;
+max = 3;
+
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * max) + 1;
+  console.log(computerChoice);
+  return computerChoice;
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt(
+    "Please enter your choice (Rock, Paper or Scissors): ",
+  );
+  console.log(humanChoice);
+  return humanChoice;
+}
+
+for (i = 0; i < 5; i++) {
+  let computerChoice = getComputerChoice();
+  let humanChoice = getHumanChoice();
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toUpperCase();
+    console.log(humanChoice);
+    switch (computerChoice) {
+      case 1:
+        if (humanChoice == "ROCK") {
+          console.log("Tied");
+        }
+        if (humanChoice == "PAPER") {
+          console.log("You win");
+          humanScore += 1;
+        }
+        if (humanChoice == "SCISSORS") {
+          console.log("You lose");
+          computerScore += 1;
+        }
+        break;
+      case 2:
+        if (humanChoice == "ROCK") {
+          console.log("You lose");
+          computerScore += 1;
+        }
+        if (humanChoice == "PAPER") {
+          console.log("Tied");
+        }
+        if (humanChoice == "SCISSORS") {
+          console.log("You win");
+          humanScore += 1;
+        }
+        break;
+      case 3:
+        if (humanChoice == "ROCK") {
+          console.log("You win");
+          humanScore += 1;
+        }
+        if (humanChoice == "PAPER") {
+          console.log("You lose");
+          computerScore += 1;
+        }
+        if (humanChoice == "SCISSORS") {
+          console.log("Tied");
+        }
+        break;
+    }
+    console.log(computerScore, humanScore);
+  }
+
+  playRound(humanChoice, computerChoice);
+}
