88e661706bbfdee0c56898339c780488f724cb92
deocze <255413163+deocze@users.noreply.github.com>
2026-04-03 14:04:50 -0400

    Logic finished

--- JavaScript&Css/JavaScript.js
+++ JavaScript&Css/JavaScript.js
@@ -2,73 +2,89 @@ let humanScore = 0,
   computerScore = 0;
 max = 3;
 
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissors = document.querySelector("#scissors");
+
+rock.addEventListener("click", (e) => playGame(e));
+paper.addEventListener("click", (e) => playGame(e));
+scissors.addEventListener("click", (e) => playGame(e));
+
+
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * max) + 1;
-  console.log(computerChoice);
   return computerChoice;
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt(
-    "Please enter your choice (Rock, Paper or Scissors): ",
-  );
-  console.log(humanChoice);
-  return humanChoice;
+function getHumanChoice(e) {
+  let text = e.target.innerText 
+  switch (text){
+    case "Rock":
+    console.log("User picked: Rock");
+    return 1;
+    case "Paper":
+      console.log("User picked: Paper");
+      return 2;
+    case "Scissors":
+      console.log("User picked: Scissors");
+      return 3;
+  }
 }
 
 
 
+function playGame(e){
+  let computerChoice = getComputerChoice();
+  let humanChoice = getHumanChoice(e);
 
-// for (let i = 0; i < 5; i++) {
-//   let computerChoice = getComputerChoice();
-//   let humanChoice = getHumanChoice();
+  playRound(humanChoice, computerChoice);
 
-//   playRound(humanChoice, computerChoice);
-
-//   function playRound(humanChoice, computerChoice) {
-//     humanChoice = humanChoice.toUpperCase();
-//     console.log(humanChoice);
-//     switch (computerChoice) {
-//       case 1:
-//         if (humanChoice == "ROCK") {
-//           console.log("Tied");
-//         }
-//         if (humanChoice == "PAPER") {
-//           console.log("You win");
-//           humanScore += 1;
-//         }
-//         if (humanChoice == "SCISSORS") {
-//           console.log("You lose");
-//           computerScore += 1;
-//         }
-//         break;
-//       case 2:
-//         if (humanChoice == "ROCK") {
-//           console.log("You lose");
-//           computerScore += 1;
-//         }
-//         if (humanChoice == "PAPER") {
-//           console.log("Tied");
-//         }
-//         if (humanChoice == "SCISSORS") {
-//           console.log("You win");
-//           humanScore += 1;
-//         }
-//         break;
-//       case 3:
-//         if (humanChoice == "ROCK") {
-//           console.log("You win");
-//           humanScore += 1;
-//         }
-//         if (humanChoice == "PAPER") {
-//           console.log("You lose");
-//           computerScore += 1;
-//         }
-//         if (humanChoice == "SCISSORS") {
-//           console.log("Tied");
-//         }
-//         break;
-//     }
-//     console.log(computerScore, humanScore);
-//   }
-// }
+  function playRound(humanChoice, computerChoice) {
+    switch (computerChoice) {
+      case 1:
+        if (humanChoice == 1) {
+          console.log("Tied");
+        }
+        if (humanChoice == 2) {
+          console.log("You win");
+          humanScore += 1;
+        }
+        if (humanChoice == 3) {
+          console.log("You lose");
+          computerScore += 1;
+        }
+        console.log("\n");
+        break;
+      case 2:
+        if (humanChoice == 1) {
+          console.log("You lose");
+          computerScore += 1;
+        }
+        if (humanChoice == 2) {
+          console.log("Tied");
+        }
+        if (humanChoice == 3) {
+          console.log("You win");
+          humanScore += 1;
+        }
+        console.log("\n");
+        break;
+      case 3:
+        if (humanChoice == 1) {
+          console.log("You win");
+          humanScore += 1;
+        }
+        if (humanChoice == 2) {
+          console.log("You lose");
+          computerScore += 1;
+        }
+        if (humanChoice == 3) {
+          console.log("Tied");
+        }
+        console.log("\n");
+        break;
+    }
+    console.log("Computer Score: %d\n", computerScore,);
+    console.log("Your Score: %d\n", humanScore)
+  }
+}
